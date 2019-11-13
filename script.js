bcd90b90d4acdf743ea4926e32b8721b267ed41c
felice7 <feliga76@gmail.com>
2019-11-13 22:04:29 +0100

    esercizio generatore password

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+  var Cognome = prompt("Inserisci il tuo cognome:");
+  
+  
+  var Nome = prompt("Inserisci il tuo nome:");
+  
+  
+  var Colore = prompt("Inserisci il tuo colore preferito:");
+  
+  
+  var risultato = (Cognome+Nome+Colore+19);
+  
+  document.getElementById("risultato").innerHTML = risultato;
